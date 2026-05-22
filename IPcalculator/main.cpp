03c80fe9d4a5e204bff35b087f74d55f50ff1d7e
КРУГ ЧЕРНЫЙ <lavrentiy36@mail.ru>
2026-05-22 21:16:41 +0300

    Prefix range almost DONE

--- IPcalculator/main.cpp
+++ IPcalculator/main.cpp
@@ -28,8 +28,11 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		freopen("CONOUT$", "w", stdout);
 		std::cout << "Init" << std::endl;
 		SetFocus(GetDlgItem(hwnd, IDC_IP_ADDRESS));
+		//https://learn.microsoft.com/en-us/windows/win32/controls/udm-setrange
+		//https://learn.microsoft.com/en-us/windows/win32/winmsg/makeword
+		SendMessage(GetDlgItem(hwnd, IDC_SPIN_PREFIX), UDM_SETRANGE, 0, MAKEWORD(30, 0));
 		break;
-
+	
 	case WM_COMMAND:
 	{
 		switch (LOWORD(wParam))
@@ -56,11 +59,13 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			if (HIWORD(wParam) == EN_CHANGE)
 			{
 				SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
+				dwIPmask &= 0xFFFFFFFC;
 				for (dwIPprefix = 0; dwIPmask; dwIPprefix++)dwIPmask <<= 1;
 				CHAR szIPprefix[3] = {};
 				sprintf(szIPprefix, "%i", dwIPprefix);
 				std::cout << szIPprefix << std::endl;
 				SendMessage(hIPprefix, WM_SETTEXT, 0, (LPARAM)szIPprefix);
+				//if(HIWORD(wParam) == EN_KILLFOCUS)SendMessage(hIPmask, IPM_SETADDRESS, 0, dwIPaddress);
 			}
 		}
 		break;
